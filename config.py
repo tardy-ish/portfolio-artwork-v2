897b1b8d96c0e6d456de13a7d92270b8b26d6a39
tardy-ish <spidey.ds@gmail.com>
2023-05-04 01:22:02 +0530

    reverted to env variables

--- config.py
+++ config.py
@@ -6,9 +6,8 @@ class Config(object):
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    fp = json.load(open('./env.variables.json'))
-    MAIL_USERNAME = fp['EMAIL']
-    MAIL_PASSWORD = fp['MAIL_PASS']
+    MAIL_USERNAME = os.environ.get('EMAIL')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_DEFAULT_SENDER = os.environ.get('EMAIL')
