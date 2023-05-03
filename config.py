e8f80ea77937064af3c66bc33f5ccf2a9b109c49
tardy-ish <spidey.ds@gmail.com>
2023-05-04 01:19:22 +0530

    updated to use json

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
+import json
 class Config(object):
     DEBUG = False
     TESTING = False
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = os.environ.get('EMAIL')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
+    fp = json.load(open('./env.variables.json'))
+    MAIL_USERNAME = fp['EMAIL']
+    MAIL_PASSWORD = fp['MAIL_PASS']
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_DEFAULT_SENDER = os.environ.get('EMAIL')
