fb73709a3f2745b8bc9a7b396bd4a9d8fb5b3fe0
tardy-ish <spidey.ds@gmail.com>
2023-05-06 04:22:20 +0530

    slight fix

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     MAIL_PORT = os.environ.get('SMTP_PORT')
     MAIL_USERNAME = os.environ.get('EMAIL')
     MAIL_PASSWORD = os.environ.get('MAIL_PASS')
-    MAIL_USE_TLS = os.environ.get('TLS')
-    MAIL_USE_SSL = os.environ.get('SSL')
+    MAIL_USE_TLS = True if os.environ.get('TLS') == "True" else False
+    MAIL_USE_SSL = True if os.environ.get('SSL') == "True" else False
     MAIL_DEFAULT_SENDER = os.environ.get('EMAIL')
 
 class ProductionConfig(Config):
