0734c15e1ede6157beefd3dcf2bfd81dbe45c663
tardy-ish <spidey.ds@gmail.com>
2023-05-18 21:52:10 +0530

    publications first layout

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ class Config(object):
     DEBUG = False
     TESTING = False
 
-    MAIL_SERVER = os.getenv('SMTP_SERVER').strip()
-    MAIL_PORT = os.getenv('SMTP_PORT').strip()
-    MAIL_USERNAME = os.getenv('UNAME').strip()
-    MAIL_PASSWORD = os.getenv('PASS').strip()
-    MAIL_USE_SSL = True if os.getenv('SSL').strip() == "True" else False
-    MAIL_USE_TLS = True if os.getenv('TLS').strip() == "True" else False
-    MAIL_DEFAULT_SENDER = os.getenv('EMAIL').strip()
+    # MAIL_SERVER = os.getenv('SMTP_SERVER').strip()
+    # MAIL_PORT = os.getenv('SMTP_PORT').strip()
+    # MAIL_USERNAME = os.getenv('UNAME').strip()
+    # MAIL_PASSWORD = os.getenv('PASS').strip()
+    # MAIL_USE_SSL = True if os.getenv('SSL').strip() == "True" else False
+    # MAIL_USE_TLS = True if os.getenv('TLS').strip() == "True" else False
+    # MAIL_DEFAULT_SENDER = os.getenv('EMAIL').strip()
 
     db_user = os.getenv('db_u').strip()
     db_pass = os.getenv('db_p').strip()
