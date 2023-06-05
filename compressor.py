d5fcb4438c8b2a7c65a71c4c690969af5fe40bc6
tardy-ish <spidey.ds@gmail.com>
2023-06-05 20:06:33 +0530

    Updated translations and contact page

--- compressor.py
+++ compressor.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 src = './static/assets/images/artworks'
-des = './static/assets/images/exhibitions_prepped'
+des = './static/assets/images/gallery_prepped'
 max_size = (800, 800)
 qual = 15
 img_list = [img for img in os.listdir(src)]
@@ -10,6 +10,7 @@ def compress(img_list: list,src :str,des :str):
     for i,file in enumerate(img_list):
         img = Image.open(f"{src}/{file}")
         img.resize(max_size,Image.LANCZOS)
+        file = file.replace(' ','_')
         img.save(f"{des}/{file}", optimize=True, quality=qual)
 
 compress(img_list=img_list,src=src,des=des)
