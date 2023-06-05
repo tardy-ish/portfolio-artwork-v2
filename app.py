8c58aa66cd6322254e7d162ab3dde7d53ae27121
tardy-ish <spidey.ds@gmail.com>
2023-06-05 23:11:43 +0530

    image modal updates

--- app.py
+++ app.py
@@ -87,11 +87,11 @@ def contact(lang):
 @app.route('/<lang>/selected_works')
 def artworks(lang):
     tls = json.load(open('./static/translations.json',encoding='utf-8'))
-    # paintings = json.load(open('./static/assets/paintings.json',encoding='utf-8'))
+    paintings = json.load(open('./static/assets/paintings.json',encoding='utf-8'))
     # paintings = artwork.query.order_by(artwork.order).all()
     # paintings = [painting.__dict__ for painting in paintings]
-    paintings = os.listdir('./static/assets/images/gallery_prepped')
-    paintings = [painting for painting in paintings if painting.endswith('.jpg')]
+    # paintings = os.listdir('./static/assets/images/gallery_prepped')
+    # paintings = [painting for painting in paintings if painting.endswith('.jpg')]
     print(paintings)
     return render_template(
         'artworks2.html',
--- static/js/artworks2.js
+++ static/js/artworks2.js
@@ -1,7 +1,13 @@
 document.addEventListener("click", function (e) {
     if (e.target.classList.contains("painting")) {
         const src = e.target.getAttribute("src");
+        const title = e.target.getAttribute("data-title");
+        const size = e.target.getAttribute("data-size");
+        const details = e.target.getAttribute("data-details");
         document.querySelector(".modal-img").src = src;
+        document.querySelector(".modal-img-title").textContent = title;
+        document.querySelector(".modal-size").textContent = size;
+        document.querySelector(".modal-details").textContent = details;
         const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
         myModal.show();
     }
